d1f2b51ddb950c8e80e4ce95692c5551e3a07906
Mykhailo Shpakov <mykhailo.shpakov@mail.polimi.it>
2023-03-04 12:30:00 +0100

    .

--- src/main/java/com/example/project2023shpakovtischer/HelloServlet.java
+++ src/main/java/com/example/project2023shpakovtischer/HelloServlet.java
@@ -1,15 +1,18 @@
 package com.example.project2023shpakovtischer;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello Mike and Ale!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
